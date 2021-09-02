b46de6983155ccc47730a3cc2e45d6f85dac6af3
Juan Rodriguez <juanrm071299@gmail.com>
2021-09-02 17:47:52 -0600

    calculadorfa

---
+++ src/calculadora.java
@@ -0,0 +1,15 @@
+public class calculadora {
+
+    public static int suma(int numero1, int numero2){
+        return numero1+numero2;
+    }
+    public static int resta(int numero1, int numero2){
+        return numero1-numero2;
+    }
+    public static int multiplicacion(int numero1, int numero2){
+        return numero1*numero2;
+    }
+    public static int division(int numero1, int numero2){
+        return numero1/numero2;
+    }
+}
