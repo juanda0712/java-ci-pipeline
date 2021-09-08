ff12a56488fbd2a558c8110bcbca68e4e58e0501
Juan Rodriguez <juanrm071299@gmail.com>
2021-09-08 01:56:42 -0600

    mod 1:56

--- main/test/TestCalculadora.java
+++ main/test/TestCalculadora.java
@@ -4,138 +4,97 @@ public class TestCalculadora extends TestCase {
 
     Calculadora calculator = new Calculadora();
 
-    public void testAddsTwoNumbers() {
+    //SUMA
+    public void testSumarDosNumeros() {
         int sum = calculator.suma(1,1);
         assertEquals(2, sum);
+    }
+    public void testFalloSumaPorParametrosNegativos(){
+        var thrown = false;
+        try{
+            calculator.suma(-2,2);
+        }catch (IllegalArgumentException e){
+            thrown = true;
+        }
+        assertTrue(thrown);
+
+        thrown = false;
+        try{
+            calculator.suma(2,-2);
+        }catch (IllegalArgumentException e){
+            thrown = true;
+        }
+        assertTrue(thrown);
+    }
+
+    //RESTA
+    public void testRestaDosNumeros() {
+        int resta = calculator.resta(4,2);
+        assertEquals(2, resta);
+    }
+    public void testFalloRestaPorParametrosNegativos(){
+        var thrown = false;
+        try{
+            calculator.resta(-3,2);
+        }catch (IllegalArgumentException e){
+            thrown = true;
+        }
+        assertTrue(thrown);
+
+        thrown = false;
+        try{
+            calculator.resta(3,-2);
+        }catch (IllegalArgumentException e){
+            thrown = true;
+        }
+        assertTrue(thrown);
+    }
+
+    //MULTIPLICACION
+    public void testMultiplicacionDosNumeros() {
+        int mult = calculator.multiplicacion(4,2);
+        assertEquals(8, mult);
+    }
+    public void testFalloMultiplicacionPorParametrosNegativos(){
+        var thrown = false;
+        try{
+            calculator.multiplicacion(-6,2);
+        }catch (IllegalArgumentException e){
+            thrown = true;
+        }
+        assertTrue(thrown);
+
+        thrown = false;
+        try{
+            calculator.multiplicacion(6,-2);
+        }catch (IllegalArgumentException e){
+            thrown = true;
+        }
+        assertTrue(thrown);
+    }
+
+    //DIVISION
+    public void testDivisionDosNumeros() {
+        int div = calculator.divisionEntera(8,2);
+        assertEquals(4, div);
+    }
+    public void testFalloDivisionPorParametrosNegativos(){
+        var thrown = false;
+        try{
+            calculator.divisionEntera(-9,3);
+        }catch (IllegalArgumentException e){
+            thrown = true;
+        }
+        assertTrue(thrown);
+
+        thrown = false;
+        try{
+            calculator.divisionEntera(9,-3);
+        }catch (IllegalArgumentException e){
+            thrown = true;
+        }
+        assertTrue(thrown);
     }
 }
 
 
-
-/*import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.CsvSource;
-
-public class CalculadoraTests{
-    Calculadora calculadora = new Calculadora();
-
-    //SUMAS
-    @Test
-    @DisplayName("1 + 1 = 2")
-    public void sumarDosNumeros() {
-        Assertions.assertEquals(2, calculadora.suma(1, 1), "1 + 1 es 2");
-    }
-
-    @ParameterizedTest(name = "{0} + {1} = {2}")
-    @CsvSource({
-            "0,    1,   1",
-            "1,    2,   3",
-            "49,  51, 100",
-            "1,  100, 101"
-    })
-    public void suma(int n1, int n2, int resultadoEsperado) {
-        Assertions.assertEquals(resultadoEsperado, calculadora.suma(n1, n2),
-                () -> n1 + " + " + n2 + " es igual a " + resultadoEsperado);
-    }
-
-    @Test
-    public void falloEnSumaPorParametrosNegativos(){
-        Assertions.assertThrows(IllegalArgumentException.class,() -> {
-            calculadora.suma(-2, 2);
-        });
-        Assertions.assertThrows(IllegalArgumentException.class,() -> {
-            calculadora.suma(2, -2);
-        });
-    }
-
-    //RESTAS
-    @Test
-    @DisplayName("10 - 3 = 7")
-    public void restarDosNumeros() {
-        Assertions.assertEquals(7, calculadora.resta(10, 3), "10 - 3 es 7");
-    }
-
-    @ParameterizedTest(name = "{0} - {1} = {2}")
-    @CsvSource({
-            "5,    1,   4",
-            "9,    4,   5",
-            "49,  51, -2",
-            "1,  100, -99"
-    })
-    public void resta(int n1, int n2, int resultadoEsperado) {
-        Assertions.assertEquals(resultadoEsperado, calculadora.resta(n1, n2),
-                () -> n1 + " - " + n2 + " es igual a " + resultadoEsperado);
-    }
-
-    @Test
-    public void falloEnRestaPorParametrosNegativos(){
-        Assertions.assertThrows(IllegalArgumentException.class,() -> {
-            calculadora.resta(-2, 2);
-        });
-        Assertions.assertThrows(IllegalArgumentException.class,() -> {
-            calculadora.resta(2, -2);
-        });
-    }
-
-
-    //MULTIPLICACION
-    @Test
-    @DisplayName("10 * 3 = 30")
-    public void multiplicarDosNumeros() {
-        Assertions.assertEquals(30, calculadora.multiplicacion(10, 3), "10 * 3 es 30");
-    }
-
-    @ParameterizedTest(name = "{0} * {1} = {2}")
-    @CsvSource({
-            "5,    1,   5",
-            "10,    4,   40",
-            "49,  51, 2499",
-            "1,  100, 100"
-    })
-    public void multiplicar(int n1, int n2, int resultadoEsperado) {
-        Assertions.assertEquals(resultadoEsperado, calculadora.multiplicacion(n1, n2),
-                () -> n1 + " * " + n2 + " es igual a " + resultadoEsperado);
-    }
-
-    @Test
-    public void falloEnMultiplicacionPorParametrosNegativos(){
-        Assertions.assertThrows(IllegalArgumentException.class,() -> {
-            calculadora.multiplicacion(-2, 2);
-        });
-        Assertions.assertThrows(IllegalArgumentException.class,() -> {
-            calculadora.multiplicacion(2, -2);
-        });
-    }
-
-
-    //DIVISION
-    @Test
-    @DisplayName("10 / 3 = 3")
-    public void dividirEnteraDosNumeros() {
-        Assertions.assertEquals(3, calculadora.divisionEntera(10, 3), "10 / 3 es 3");
-    }
-
-    @ParameterizedTest(name = "{0} / {1} = {2}")
-    @CsvSource({
-            "5,    1,   5",
-            "9,    4,   2",
-            "49,  51, 0",
-            "1,  100, 0"
-    })
-    public void division(int n1, int n2, int resultadoEsperado) {
-        Assertions.assertEquals(resultadoEsperado, calculadora.divisionEntera(n1, n2),
-                () -> n1 + " / " + n2 + " es igual a " + resultadoEsperado);
-    }
-
-    @Test
-    public void falloEnDivisionPorParametrosNegativos(){
-        Assertions.assertThrows(IllegalArgumentException.class,() -> {
-            calculadora.divisionEntera(-2, 2);
-        });
-        Assertions.assertThrows(IllegalArgumentException.class,() -> {
-            calculadora.divisionEntera(2, -2);
-        });
-    }
-}*/
